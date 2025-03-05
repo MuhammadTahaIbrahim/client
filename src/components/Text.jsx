2d17d6e1e7274196a38aa54b3a3e39e0aa2c34ff
MuhammadTahaIbrahim <muhammadtaha3713@gmail.com>
2025-03-06 01:33:47 +0500

    Update Text.jsx

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -6,10 +6,28 @@ const Text = () => {
     const [editMode, setEditMode] = useState(false);
     const [val, seVal] = useState("double click to edit");
 
+    const handleEditClick = () => {
+        setEditMode(true);
+    };
+
+    const handleBlur = () => {
+        setEditMode(false);
+    };
+
     return ( 
         <Draggable>
             {
-                editMode ? ( <input onDoubleClick={(e) => setEditMode(false)} value={val} onChange={(e) => seVal(e.target.value)} /> ) : ( <h5 onDoubleClick={(e) => setEditMode(true)}>{val}</h5> )
+                // editMode ? ( <input onDoubleClick={(e) => setEditMode(false)} value={val} onChange={(e) => seVal(e.target.value)} /> ) : ( <h5 onDoubleClick={(e) => setEditMode(true)}>{val}</h5> )
+
+                editMode ? (
+                    <input 
+                        onBlur={handleBlur} 
+                        value={val} 
+                        onChange={(e) => seVal(e.target.value)} 
+                    />
+                ) : (
+                    <h5 onClick={handleEditClick}>{val}</h5>
+                )
             }
         </Draggable>
     );
